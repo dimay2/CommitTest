23e468183ea772b3b833e253bde8ff1d5771899f
dimay2 <dimay2@hotmail.com>
2026-02-12 20:07:38 +0200

    cleanup route53.tf

--- commitlab-infra/route53.tf
+++ commitlab-infra/route53.tf
@@ -19,20 +19,3 @@ resource "aws_route53_record" "argocd" {
   ttl     = 300
   records = [kubernetes_ingress_v1.argocd.status.0.load_balancer.0.ingress.0.hostname]
 }
-
-# Fetch the Frontend Ingress (deployed via Helm) to get its ALB hostname
-data "kubernetes_ingress_v1" "frontend" {
-  metadata {
-    name      = "lab-app"
-    namespace = "default"
-  }
-}
-
-# Create a CNAME record for the Frontend Application
-resource "aws_route53_record" "frontend" {
-  zone_id = aws_route53_zone.private.zone_id
-  name    = "Lab-commit-task.commit.local"
-  type    = "CNAME"
-  ttl     = 300
-  records = [data.kubernetes_ingress_v1.frontend.status.0.load_balancer.0.ingress.0.hostname]
-}
