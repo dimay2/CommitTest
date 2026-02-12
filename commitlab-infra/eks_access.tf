eacbb636211c0903cdf5d9c31d114826f565bbbe
dimay2 <dimay2@hotmail.com>
2026-02-12 18:12:47 +0200

    eks_access.tf: added data "aws_iam_role"

--- commitlab-infra/eks_access.tf
+++ commitlab-infra/eks_access.tf
@@ -1,7 +1,13 @@
+# Fetch the CodeBuild IAM role. 
+# NOTE: Ensure the 'name' matches your actual CodeBuild service role name in IAM.
+data "aws_iam_role" "codebuild" {
+  name = "${var.app_name}-codebuild-service-role" 
+}
+
 # Allow CodeBuild to access the EKS Cluster
 resource "aws_eks_access_entry" "codebuild" {
   cluster_name      = var.cluster_name
-  principal_arn     = aws_iam_role.codebuild.arn
+  principal_arn     = data.aws_iam_role.codebuild.arn
   type              = "STANDARD"
 }
 
@@ -9,7 +15,7 @@ resource "aws_eks_access_entry" "codebuild" {
 resource "aws_eks_access_policy_association" "codebuild_admin" {
   cluster_name      = var.cluster_name
   policy_arn        = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-  principal_arn     = aws_iam_role.codebuild.arn
+  principal_arn     = data.aws_iam_role.codebuild.arn
   access_scope {
     type = "cluster"
   }
