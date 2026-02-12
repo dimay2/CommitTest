f0dde92b3dc9536e3352a2692e15ab5568fae74d
dimay2 <dimay2@hotmail.com>
2026-02-12 13:35:58 +0200

    pipeline.tf image replaced to ECR custom dockerfile

--- commitlab-infra/pipeline.tf
+++ commitlab-infra/pipeline.tf
@@ -95,7 +95,7 @@ resource "aws_codebuild_project" "app_build" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
+    image                       = "${aws_ecr_repository.codebuild_builder.repository_url}:latest"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
     privileged_mode             = true # Required for Docker
